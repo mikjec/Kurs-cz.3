0063396fbe2c57a6ecbd27504a95cdf9e138af00
Mikolaj <mikolaj.jecz@gmail.com>
2024-11-03 19:52:19 +0100

    adding BEM and JS

---
+++ JavaScript/Nowy folder/script.js
@@ -0,0 +1,10 @@
+console.log('cze')
+const tab = [1, 2, 1, 1]
+console.log(`%cczesc skibididd ${tab[1]}`, 'font-weight:bold; color:red;')
+
+function wyposz(imie, nazwisko) {
+	// alert(`imie: ${imie}, nazwisko: ${nazwisko}`)
+	console.log(`imie: ${imie}, nazwisko: ${nazwisko}`)
+}
+
+wyposz('skibidi', 'rizzala')
